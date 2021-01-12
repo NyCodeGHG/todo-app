c343940474eee874701e773d4630598e0cab79df
NyCodeGHG <nycode@protonmail.com>
2021-01-12 15:30:37 +0100

    Fixed fab button and added another example

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { Add as AddIcon } from "@material-ui/icons";
 
 const useStyles = makeStyles({
     'add-button': {
-        position: 'absolute',
-        right: 16,
+        position: 'fixed',
+        left: 16,
         bottom: 16
     }
 });
@@ -22,6 +22,13 @@ function App() {
             information: 'Buy some apples in the supermarket.',
             dueDate: new Date(2021, 1, 15, 15, 3),
             done: false
+        },
+        {
+            id: 1,
+            title: 'Do homework',
+            information: 'Do my homework I got from my school. This is a longer description. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+            dueDate: new Date(2021, 1, 15, 15, 3),
+            done: true
         }
     ]);
 
